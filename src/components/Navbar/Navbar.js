fbcbe3a7592eba2f8dcc0863d3b65ac34a0b1e62
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-19 04:35:55 -0500

    --- Auto-Git Commit ---

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,30 @@
 import { Avatar } from '@material-ui/core'
+import { SearchOutlined } from '@material-ui/icons'
 import React from 'react'
 import './Navbar.css'
+import logo from './netflix-logo.png'
 
 function Navbar() {
     return (
         <div className="navbar">
             <img 
              className="navbar__logo"
-             src="https://banner2.cleanpng.com/20180606/lca/kisspng-netflix-logo-t-shirt-television-fonts-5b1798850adbc6.2599171615282730290445.jpg"
+             src={logo}
              alt="Netflix Logo"
             /> 
 
+            <div className="navbar__search">
+                <input 
+                 variant="outlined"
+                 type="search"
+                 color="secondary"
+                />
+                <SearchOutlined
+                 className="navbar__searchIcon"                 
+                 fontSize="large" 
+                />  
+            </div>
+            
             <Avatar
              className="navbar__avatar"
              src="https://placeimg.com/640/480/people"
